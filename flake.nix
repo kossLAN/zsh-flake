30c3b61717777acb24ad68ca6badbcf8fd538e5e
kossLAN <kosslan@kosslan.dev>
2024-11-14 23:35:09 -0500

    overlay testing

--- flake.nix
+++ flake.nix
@@ -14,7 +14,12 @@
       ["x86_64-linux" "aarch64-linux"]
       (system: fn system nixpkgs.legacyPackages.${system});
   in {
-    # Define a package for your project
+    # Overlay for using this in system configurations.
+    overlays.default = forEachSystem (system: (final: prev: {
+      zsh = self.packages.zsh.system.default;
+    }));
+
+    # Package so you can test this out, without installing :3.
     packages = forEachSystem (system: pkgs: rec {
       zsh = import ./nix/wrapZsh.nix {
         inherit pkgs;
